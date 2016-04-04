374a476257530aa3c90befad2bb8d52d954c5dc2
abdi07 <abdimahamed07@gmail.com>
2016-04-04 00:04:55 +0000

    revert test

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ router.post("/signin", function(req,res,next) {
         url: 'ldap://mccnet.mccneb.edu',
         baseDN: 'dc=mccnet,dc=mccneb,dc=edu',
         username: 'HORTTEST@mccnet.mccneb.edu',
-        password: 'H0rtG3t1n!'
+        password: req.body.password
     }
 
     var ad = new ActiveDirectory(config);
