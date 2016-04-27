55e1fa8421745222212e4d5e0e8d3048998d1109
abdi42 <abdullahimahamed42@gmail.com>
2016-04-27 16:50:37 -0500

    Update plant.js

--- models/plant.js
+++ models/plant.js
@@ -1,13 +1,12 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
 var PlantSchema = new Schema({
-    commonName:  { type: String, default: '' } // required,
-    genus : { type: String, default: '' } // required,
-    species: { type: String, default: '' } // required,
+    commonName:  { type: String, default: '' }, // required,
+    genus : { type: String, default: '' }, // required,
+    species: { type: String, default: '' }, // required,
     variety :{ type: String, default: '' },
-    family : { type: String, default: '' } //required,
+    family : { type: String, default: '' }, //required,
     plantPropagation : { type: String, default: '' },
     zoneHardiness : { type: Number, default: 0},
     exposure: { type: String, default: '' },
@@ -16,13 +15,14 @@ var PlantSchema = new Schema({
     insects : { type: String, default: '' },
     diseases : { type: String, default: '' },
     classification : { type: String, default: '' },
-    campus : { type: String, default: '' } //required,
-    height : { type: Number, default: 0 },
+    campus : { type: String, default: '' }, //required,
+    heights : [],
     spread : { type: Number, default: 0 },
     flowers : { type: String, default: '' },
     fruit : { type: String, default: '' },
     latitude:{ type: Number, default: 0 },
-    longitude:{ type: Number, default: 0 }
+    longitude:{ type: Number, default: 0 },
+    dead:{type:Boolean, default:false}
 })
 
 module.exports = mongoose.model("Plant",PlantSchema);
